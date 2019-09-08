3678b5f91422218c5c2dd48362d2707eedc5922b
kivang <863021085@qq.com>
2019-09-08 17:05:52 +0800

    陈小封继续完善页面

--- js/index.js
+++ js/index.js
@@ -9,18 +9,35 @@ $(function(){
 	})
 
 		$("ul.left_nav > li").mouseenter(function(){
- 		$(this.firstChild).addClass("current_a");
+ 		$(this.firstChild.nextSibling).addClass("current_a");
 	})
 
 		$("ul.left_nav > li").mouseout(function(){
-   		$(this.firstChild).removeClass("current_a");
+   		$(this.firstChild.nextSibling).removeClass("current_a");
 	})
 		$("ul.left_nav > li > a").mouseenter(function(){
  		$(this).addClass("current_a");
+ 		$(this.previousSibling).addClass("current_i");
 	})
 
 		$("ul.left_nav > li > a").mouseout(function(){
    		$(this).removeClass("current_a");
+   		$(this.previousSibling).addClass("current_i");
+	})
+		$("ul.left_nav > li").mouseenter(function(){
+ 		$(this.firstChild).addClass("current_i");
+	})
+
+		$("ul.left_nav > li").mouseout(function(){
+   		$(this.firstChild).removeClass("current_i");
+	})
+		$("ul.left_nav > li > i").mouseenter(function(){
+ 		$(this).addClass("current_i");
+ 		$(this.nextSibling).addClass("current_a");
+	})
+		$("ul.left_nav > li > i").mouseout(function(){
+ 		$(this).addClass("current_i");
+ 		$(this.nextSibling).addClass("current_a");
 	})
 		
 	// 默认选中第一个li
